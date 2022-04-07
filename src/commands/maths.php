79b05115faf64c29196d2211b08c02482b4a7f80
Nirbose <nirbose2@gmail.com>
2022-04-07 18:28:49 +0200

    add chapiter
    Co-Authored-By: oxyblade <73794674+oxyblade-1@users.noreply.github.com>

--- src/commands/maths.php
+++ src/commands/maths.php
@@ -7,7 +7,7 @@ use Sti2d\Revisobot\App\Commands\Create as Command;
 use Sti2d\Revisobot\App\Commands\Option;
 use Sti2d\Revisobot\App\Commands\OptionChoice;
 
-Command::new('maths', 'salut')
+Command::new('maths', 'Commande pour les chapitres de Maths')
 ->setExec(function (Interaction $i) {
     $i->respondWithMessage(
         MessageBuilder::new()->setContent('Ceci est un test')
@@ -15,7 +15,16 @@ Command::new('maths', 'salut')
 })
 ->setIsSlash(true)
 ->setOptions(
-    Option::new('a', 'a')->setType(CommandOption::STRING)->addChoices(
-        OptionChoice::new('test', 'truc')
+    Option::new('chapitres', 'Tout les chapitres de Maths')->setRequired(true)->setType(CommandOption::INTEGER)->addChoices(
+        OptionChoice::new('Automatisme', 1),
+        OptionChoice::new('Dérivée et primitive', 2),
+        OptionChoice::new('Suites', 3),
+        OptionChoice::new('Foncitons inverses', 4),
+        OptionChoice::new('Fonctions exponentielle', 5),
+        OptionChoice::new('Equation différentielle', 6),
+        OptionChoice::new('Logarithme népérien', 7),
+        OptionChoice::new('Probabilités conditionnelles', 8),
+        OptionChoice::new('Nombre complexe', 9),
+        OptionChoice::new('Les log10', 10),
     ))
 ->setGuilds('773468731702640661', '781105165754433537');
---
+++ src/commands/physique.php
@@ -0,0 +1,33 @@
+<?php
+
+use Discord\Builders\MessageBuilder;
+use Discord\Parts\Interactions\Command\Option as CommandOption;
+use Discord\Parts\Interactions\Interaction;
+use Sti2d\Revisobot\App\Commands\Create as Command;
+use Sti2d\Revisobot\App\Commands\Option;
+use Sti2d\Revisobot\App\Commands\OptionChoice;
+
+Command::new('physique', 'Commande pour les chapitres de Physique')
+->setExec(function (Interaction $i) {
+    $i->respondWithMessage(
+        MessageBuilder::new()->setContent('Ceci est un test')
+    );
+})
+->setIsSlash(true)
+->setOptions(
+    Option::new('chapitres', 'Tout les chapitres de Physique')->setRequired(true)->setType(CommandOption::INTEGER)->addChoices(
+        OptionChoice::new('Automatisme', 1),
+        OptionChoice::new('Energie solaire/transfert thermique', 2),
+        OptionChoice::new('Combustion (bilan énergétique)', 3),
+        OptionChoice::new('Oxydoréduction', 4),
+        OptionChoice::new('Batterie au plomb', 5),
+        OptionChoice::new('Décharge d\'accumulateur', 6),
+        OptionChoice::new('Réaction acido/basique + le PH', 7),
+        OptionChoice::new('Mouvement des forces/forces mecanique', 8),
+        OptionChoice::new('Grandeur sinusoïdales', 9),
+        OptionChoice::new('Transfert thermique', 10),
+        OptionChoice::new('Ondes sonnores', 11),
+        OptionChoice::new('Rappel des angles', 12),
+        OptionChoice::new('Vitesse linéaire/vitesse angulaire', 13),
+    ))
+->setGuilds('773468731702640661', '781105165754433537');
